c194336982e15cf8985fe4a37101a2a5639337fd
ranan grobman <binyamin.grobman@gmail.com>
2019-01-10 15:29:00 +0200

    some changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 const card = document.querySelectorAll('.card');
 const cardTitle = document.querySelectorAll('.card-title');
-const search = document.getElementById('search');
+const sr = document.querySelectorAll('#search');
+
 function getData(url){
 return  fetch(url)
        .then(res => res.json())
@@ -15,7 +16,13 @@ var html = '<select>' ;
   var  op =  $.each(data,function( key, value ) {
       html += '<option value=' + value +'>' + value +'</option>';
   })
- search.innerHTML =  html  + '</select>';
+
+ $('#search').html(html +  '</select>');
+}
+function getImg(data,element,cl){
+  var img = document.createElement('div');
+  img.innerHTML = (`<img class='${cl}' src='${data}' alt="Image of the dog">`);
+  element.prepend(img);
 }
 
  for(let i = 0; i <card.length; i +=1 ){
@@ -23,21 +30,29 @@ var html = '<select>' ;
   getData(url)
          .then(data => data.message)
          .then( (data) => {
-          var img = document.createElement('div');
-          img.innerHTML = (`<img class="card-img-top" src='${data}' alt="Image of the dog">`);
-          card[i].prepend(img);
-         });
+          getImg(data , card[i],  'card-img-top');
+        });
  }
+
+
 
 getData('https://dog.ceo/api/breeds/list')
 .then(data => generateOptions(data.message));
 
 
-
-  getData(`https://dog.ceo/api/breed/${$('select').val()}/images/random`)
+//random img
+for(let i = 0 ; i < 3; i ++){
+  getData('https://dog.ceo/api/breeds/image/random')
   .then(data => data.message)
-  .then(data => {
-   var img = document.createElement('div');
-   img.innerHTML = `<img src='${data}' alt="random img of selected dog">`;
-   $('#search').html(img);
+  .then( (data) => {
+   getImg(data , $('#random'),'img-fluid-random');
   });
+}
+
+
+// select img
+getData(`https://dog.ceo/api/breed/${$('select').val()}/images/random`)
+.then(data => data.message)
+.then( (data) => {
+ getImg(data , search,'img-fluid-random');
+});
